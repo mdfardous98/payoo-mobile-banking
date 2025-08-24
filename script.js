9185687c1edb9d35e46094a87dd00999e5ca860c
mdfardous98 <mdjfardous@gmail.com>
2025-08-24 22:53:13 +0600

    added navbar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Login Button Functionality
 
-document.getElementById("loginButton").
-addEventListener("click",function(e){
-e.preventDefault()
-    console.log("login Button Clicked");
-    console.log(e);
-})
+document.getElementById("loginButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+    window.location.href="./home.html"
+  }
+  else{
+    alert("invalid")
+  }
+});
